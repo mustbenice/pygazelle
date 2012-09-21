eec2e3848a58cac8096a36e49d39053f06b77e1c
Aaron Cohen <cohena@gmail.com>
2012-09-20 22:48:03 -0700

    Starting work on torrent

--- torrent.py
+++ torrent.py
@@ -1,5 +1,7 @@
-import api
+class InvalidTorrentException(Exception):
+    pass
 
 class Torrent(object):
-    def __init__(self):
-        pass
+    def __init__(self, id, parent_api):
+        self.id = id
+        self.parent_api = parent_api
